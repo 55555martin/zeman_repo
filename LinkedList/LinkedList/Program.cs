5861648f6fa748a3344005047f8e413ed7e7dfe6
55555martin <zemanm@gchd.cz>
2024-10-16 15:54:48 +0200

    update

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -10,6 +10,46 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
+            Node uzl = new Node(12);
+        }
+    }
+    class Node
+    {
+        public Node(int value) // konstruktor
+        {
+            Value = value;
+        }
+        public int Value { get; }
+        public Node Next { get; set; }
+    }
+    class LinkedList
+    {
+        public Node Head { get; set; }
+        public void Add(int value)
+        {
+            if (Head==null)
+            {
+                Head = new Node(value);
+            }
+            else
+            {
+                Node newNode = new Node(value);
+                newNode.Next = Head;
+                Head = newNode;
+            }
+        }
+        public bool Find(int value)
+        {
+            Node prvek = Head;
+            while (prvek!=null)
+            {
+                if(prvek.Value == value)
+                {
+                    return true;
+                }
+                prvek = prvek.Next;
+            }
+            return false;
         }
     }
 }
