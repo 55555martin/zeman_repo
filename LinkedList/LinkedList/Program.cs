09cca148ba7bd31f8b75ba3356592eeccea64a14
Martin Zeman <zemanm@gchd.cz>
2024-10-22 17:52:30 +0200

    přidání funkce Min()

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -25,7 +25,7 @@ namespace LinkedList
     class LinkedList
     {
         public Node Head { get; set; }
-        public void Add(int value)
+        public void Add(int value) // Přidá prvek na začátek seznamu
         {
             if (Head==null)
             {
@@ -37,8 +37,9 @@ namespace LinkedList
                 newNode.Next = Head;
                 Head = newNode;
             }
-        }
-        public bool Find(int value)
+        } // složitost O(1)
+
+        public bool Find(int value) // Vrátí true, pokud se v seznamu hledaný prvek nachází, jinak false
         {
             Node prvek = Head;
             while (prvek!=null)
@@ -50,6 +51,21 @@ namespace LinkedList
                 prvek = prvek.Next;
             }
             return false;
-        }
+        } // složitost O(n)
+
+        public int Min() // Najde minimum v seznamu, pokud je seznam prázdný vrátí null
+        {
+            int minimum = Head.Value;
+            Node prvek = Head.Next;
+            while (prvek != null)
+            {
+                if(prvek.Value < minimum)
+                {
+                    minimum = prvek.Value;
+                }
+                prvek = prvek.Next;
+            }
+            return minimum;
+        } // složitost O(n)
     }
 }
