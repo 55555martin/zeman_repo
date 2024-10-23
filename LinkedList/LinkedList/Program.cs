baa5396c93ba73dd14a8bd4b2015c4ba1349a6c5
Martin Zeman <zemanm@gchd.cz>
2024-10-23 15:58:36 +0200

    update

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -10,7 +10,17 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            Node uzl = new Node(12);
+            LinkedList a = new LinkedList();
+            a.Add(5);
+            a.Add(4);
+            a.Add(3);
+            a.Add(12);
+            a.Add(5);
+            a.Add(6);
+            a.PrintLinkedList();
+            a.SortLinkedList();
+            a.PrintLinkedList();
+            Console.ReadLine();
         }
     }
     class Node
@@ -24,7 +34,12 @@ namespace LinkedList
     }
     class LinkedList
     {
+        public LinkedList() // konstruktor
+        {
+            Length = 0;
+        }
         public Node Head { get; set; }
+        public int Length { get; set; }
         public void Add(int value) // Přidá prvek na začátek seznamu
         {
             if (Head==null)
@@ -37,6 +52,7 @@ namespace LinkedList
                 newNode.Next = Head;
                 Head = newNode;
             }
+            Length++;
         } // složitost O(1)
 
         public bool Find(int value) // Vrátí true, pokud se v seznamu hledaný prvek nachází, jinak false
@@ -66,6 +82,61 @@ namespace LinkedList
                 prvek = prvek.Next;
             }
             return minimum;
+        } // složitost O(n)
+
+        public void PrintLinkedList() // Vypíše list
+        {
+            Node prvek = Head;
+            while (prvek != null)
+            {
+                Console.Write(prvek.Value);
+                Console.Write("; ");
+                prvek = prvek.Next;
+            }
+            Console.WriteLine();
+        } // složitost O(n)
+
+        public void SortLinkedList() // seřadí list
+        {
+            LinkedList novylist = new LinkedList();
+            novylist.Head = new Node(Head.Value);
+            if (Head != null)
+            {
+                Node prvek = Head.Next;
+                while (prvek != null)
+                {
+                    novylist.InsertSorted(prvek.Value);
+                    prvek = prvek.Next;
+                }
+            }
+            Head = novylist.Head;
+        } // složitost O(n^2)
+
+        public void InsertSorted(int value) // přidá prvek do seřazeného listu na správné místo
+        {
+            if (Head == null)
+                Head = new Node(value);
+            else
+            {
+                Node prvek = Head;
+                Node predchoziprvek = null;
+                while (true)
+                {
+                    if (prvek.Value < value)
+                        if (prvek.Next != null)
+                        {
+                            predchoziprvek = prvek;
+                            prvek = prvek.Next;
+                        }
+                        else
+                            break;
+                    else
+                        break;
+                }
+                Node newNode = new Node(value);
+                newNode.Next = predchoziprvek.Next;
+                predchoziprvek.Next = newNode;
+            }
         } // složitost O(n)
     }
 }
