3cad430176f9889b9d90c360159e8bd60a454a23
Martin Zeman <zemanm@gchd.cz>
2024-11-05 20:05:04 +0100

    Práce se spojovými seznamy 2

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +15,55 @@ namespace LinkedList
             a.Add(5);
             a.Add(4);
             a.Add(3);
-            a.Add(12);
+            a.Add(1);
             a.Add(5);
             a.Add(6);
+            LinkedList b = new LinkedList();
+            b.Add(9);
+            b.Add(9);
+            b.Add(0);
+            b.Add(1);
             a.PrintLinkedList();
-            a.SortLinkedList();
+            b.PrintLinkedList();
+            Secti(a,b,10).PrintLinkedList();
             a.PrintLinkedList();
             Console.ReadLine();
         }
+
+        static LinkedList Secti(LinkedList listA, LinkedList listB, int ciselnaSoustava) // Sečte dvě čísla interpretovaná spojovým seznamem
+        // Head je číslice na místě jednotek (takže číslo je jakoby naopak)
+        {
+            LinkedList listC = new LinkedList();
+            Node a = listA.Head;
+            Node b = listB.Head;
+            listC.Head = new Node(0);
+            Node c = listC.Head;
+            int prebytek = 0;
+            while (a != null || b != null)
+            {
+                if (a == null)
+                {
+                    c.Next = new Node(b.Value+prebytek);
+                    prebytek = 0;
+                    b = b.Next;
+                }
+                else if (b == null)
+                {
+                    c.Next = new Node(a.Value+prebytek);
+                    prebytek = 0;
+                    a = a.Next;
+                }
+                else
+                {
+                    c.Next = new Node( (a.Value+b.Value) % ciselnaSoustava + prebytek);
+                    prebytek = (a.Value + b.Value) / ciselnaSoustava; // mělo by to být 0 či 1, pokud prvky v seznamech korespondují se soustavou
+                    a = a.Next; b = b.Next;
+                }
+                c = c.Next;
+            }
+            listC.Head = listC.Head.Next;
+            return listC;
+        } // složitost O(n)
     }
     class Node
     {
@@ -34,12 +76,7 @@ namespace LinkedList
     }
     class LinkedList
     {
-        public LinkedList() // konstruktor
-        {
-            Length = 0;
-        }
         public Node Head { get; set; }
-        public int Length { get; set; }
         public void Add(int value) // Přidá prvek na začátek seznamu
         {
             if (Head==null)
@@ -52,7 +89,6 @@ namespace LinkedList
                 newNode.Next = Head;
                 Head = newNode;
             }
-            Length++;
         } // složitost O(1)
 
         public bool Find(int value) // Vrátí true, pokud se v seznamu hledaný prvek nachází, jinak false
@@ -69,8 +105,10 @@ namespace LinkedList
             return false;
         } // složitost O(n)
 
-        public int Min() // Najde minimum v seznamu, pokud je seznam prázdný vrátí null
+        public int? Min() // Najde minimum v seznamu, pokud je seznam prázdný vrátí null
         {
+            if (Head == null)
+                return null;
             int minimum = Head.Value;
             Node prvek = Head.Next;
             while (prvek != null)
@@ -99,9 +137,9 @@ namespace LinkedList
         public void SortLinkedList() // seřadí list
         {
             LinkedList novylist = new LinkedList();
-            novylist.Head = new Node(Head.Value);
             if (Head != null)
             {
+                novylist.Head = new Node(Head.Value);
                 Node prvek = Head.Next;
                 while (prvek != null)
                 {
@@ -123,20 +161,90 @@ namespace LinkedList
                 while (true)
                 {
                     if (prvek.Value < value)
+                    {
+                        predchoziprvek = prvek;
                         if (prvek.Next != null)
-                        {
-                            predchoziprvek = prvek;
                             prvek = prvek.Next;
-                        }
                         else
                             break;
+                    }
                     else
                         break;
                 }
                 Node newNode = new Node(value);
-                newNode.Next = predchoziprvek.Next;
-                predchoziprvek.Next = newNode;
+                if (predchoziprvek == null)
+                {
+                    Head = newNode;
+                    newNode.Next = prvek;
+                }
+                else
+                {
+                    newNode.Next = predchoziprvek.Next;
+                    predchoziprvek.Next = newNode;
+                }
             }
         } // složitost O(n)
+
+        public void DestruktivniPrunik(LinkedList list)
+        {
+            SortLinkedList();
+            DestroyDuplicites();
+            list.SortLinkedList();
+            list.DestroyDuplicites();
+            Node prvek = list.Head;
+            while (prvek != null)
+            {
+                InsertSorted(prvek.Value);
+                prvek = prvek.Next;
+            }
+            // ty prvky, které se zde nacházejí 2x, jsou průnikem
+            Node predchoziprvek = null;
+            prvek = Head;
+            while (prvek != null)
+            {
+                if (prvek.Next == null || prvek.Value != prvek.Next.Value)
+                {
+                    if (predchoziprvek == null)
+                        Head = prvek.Next;
+                    else
+                        predchoziprvek.Next = prvek.Next;
+                }
+                else if (prvek.Value == prvek.Next.Value)
+                {
+                    predchoziprvek = prvek;
+                    prvek.Next = prvek.Next.Next;
+                }
+                prvek = prvek.Next;
+            }
+        } // složitost O(n^2)
+
+        public void DestruktivniSjednoceni(LinkedList list)
+        {
+            SortLinkedList();
+            Node prvek = list.Head;
+            while (prvek != null)
+            {
+                InsertSorted(prvek.Value);
+                prvek = prvek.Next;
+            }
+            DestroyDuplicites();
+        } // složitost O(n^2)
+
+        public void DestroyDuplicites() // Odstraní duplikáty v SEŘAZENÉM listu
+        {
+            Node prvek = Head;
+            while (prvek != null)
+            {
+                if (prvek.Next == null)
+                    prvek = prvek.Next;
+                else
+                {
+                    if (prvek.Value == prvek.Next.Value)
+                        prvek.Next = prvek.Next.Next;
+                    else
+                        prvek = prvek.Next;
+                }
+            }
+        } // složitost O(n) (avšak musí být seřazený)
     }
 }
