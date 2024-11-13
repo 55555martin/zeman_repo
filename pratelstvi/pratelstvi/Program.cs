495e3ed415add50152a6c9df3ad9967b60781e12
Martin Zeman <zemanm@gchd.cz>
2024-11-13 20:13:16 +0100

    Dokončení úkolu: Řetízek přátelství

--- pratelstvi/pratelstvi/Program.cs
+++ pratelstvi/pratelstvi/Program.cs
@@ -13,14 +13,14 @@ namespace pratelstvi
         {
             int n = Int32.Parse(Console.ReadLine()); // Počet lidí
             List<int>[] kamaradi = new List<int>[n+1];
-            for (int i = 0; i < n+1; i++)
+            for (int x = 0; x < n+1; x++) // n+1 je protože lidé jsou indexováni od 1 do n, nikoliv od 0 do n-1, jak to funguje v prog. jazycích
             {
-                kamaradi[i] = new List<int>();
+                kamaradi[x] = new List<int>();
             }
             string[] pratelstvi = Console.ReadLine().Split(' ');
-            foreach (string i in pratelstvi)
+            foreach (string x in pratelstvi)
             {
-                string[] spoj = i.Split('-');
+                string[] spoj = x.Split('-');
                 int i1 = Int32.Parse(spoj[0]);
                 int i2 = Int32.Parse(spoj[1]);
                 kamaradi[i1].Add(i2);
@@ -33,21 +33,57 @@ namespace pratelstvi
             List<int[]> cesty = new List<int[]>();
             int pocetcest = 0;
             projite.Add(start);
-            foreach (int i in kamaradi[start])
+            foreach (int x in kamaradi[start])
             {
-                projite.Add(i);
+                projite.Add(x);
                 int[] cesta = new int[2];
                 cesta[0] = start;
-                cesta[1] = i;
+                cesta[1] = x;
                 cesty.Add(cesta);
+                pocetcest++;
             }
             int i = 0;
-            while (i<cesty.Count())
+            int[] konecnaCesta = null;
+            while (i<pocetcest)
             {
                 int[] cesta = cesty[i];
+                int delka = cesta.Length;
                 int prvek = cesta.Last();
-
+                foreach (int x in kamaradi[prvek])
+                {
+                    if (!projite.Contains(x))
+                    {
+                        projite.Add(x);
+                        int[] novaCesta = new int[delka+1];
+                        for (int y = 0; y < delka; y++)
+                        {
+                            novaCesta[y] = cesta[y];
+                        }
+                        novaCesta[delka] = x;
+                        cesty.Add(novaCesta);
+                        pocetcest++;
+                        if (x == cil)
+                        {
+                            konecnaCesta = novaCesta;
+                            break;
+                        }
+                    }
+                }
+                if (konecnaCesta != null)
+                    break;
+                i++;
             }
-        }
-    }
-}
+            if (konecnaCesta == null)
+                Console.WriteLine("neexistuje");
+            else
+            {
+                foreach (int x in konecnaCesta)
+                {
+                    Console.Write(x.ToString()+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        } // časová složitost O(n^2)
+    }     // paměťová složitost string[] pratelstvi-O(m); List<int>[] kamaradi-O(2m); List<int> projite-O(n); List<int[]> cesty-O(n*n)
+}         //                    celkem -> O(n^2+m)
