a42325ed6907aee5a6a77710568fa069952c5915
55555martin <zemanm@gchd.cz>
2025-01-15 15:54:21 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Union-find/Union-find/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Union_find
+{
+    internal class Program
+    {
+        public class UnionFind
+        {
+            private int[] parent;
+            private int[] hloubka;
+            public UnionFind(int n)
+            {
+
+                parent = new int[n];
+                hloubka = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                    hloubka[i] = 0;
+                }
+            }
+
+            public int Koren(int vrchol)
+            {
+                while (parent[vrchol] != vrchol)
+                {
+                    vrchol = parent[vrchol];
+                }
+                return vrchol;
+            }
+
+            public bool Find(int u, int v)
+            {
+                if (Koren(u) == Koren(v))
+                    return true;
+                return false;
+            }
+
+            public void Union(int u, int v)
+            {
+                int a = Koren(u);
+                int b = Koren(v);
+                if (a != b)
+                {
+                    if (hloubka[a] < hloubka[b])
+                        parent[a] = b;
+                    else if (hloubka[b] < hloubka[a])
+                        parent[b] = a;
+                    else
+                    {
+                        parent[b] = a;
+                        hloubka[a]++;
+                    }
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int[,] Kruskal(List<int[]> hrany, int n)
+        {
+            int?[,] kostra = new int?[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    kostra[i, j] = null;
+                }
+            }
+            hrany.Sort((sa1, sa2) => sa1[2].CompareTo(sa2[2]));
+        }
+    }
+}
--- zasobnik/zasobnik/Program.cs
+++ zasobnik/zasobnik/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
@@ -107,3 +108,72 @@ namespace zasobnik
         }
     }
 }
+=======
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace zasobnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ControlBrackets(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+        static bool ControlBrackets(string text)
+        {
+            Stack<char> list = new Stack<char>();
+            foreach (char c in text)
+            {
+                if (c == '('| c == '[' | c == '{' )
+                {
+                    list.Push(c);
+                }
+                else
+                {
+                    if (list.Any())
+                    {
+                        char last = list.Pop();
+                        if (last == InvertBrackets(c))
+                        {
+                            continue;
+                        }
+                    }
+                    return false;
+                }
+            }
+            if (!list.Any())
+                return true;
+            return false;
+        }
+
+        static char InvertBrackets(char m)
+        {
+            switch (m)
+            {
+                case '(':
+                    return ')';
+                case ')':
+                    return '(';
+                case '[':
+                    return ']';
+                case ']':
+                    return '[';
+                case '{':
+                    return '}';
+                case '}':
+                    return '{';
+                default:
+                    return m;
+            }
+        }
+    }
+}
+>>>>>>> Stashed changes
