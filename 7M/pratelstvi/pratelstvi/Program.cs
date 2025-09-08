9f84138d17cb16786794babbfab682d6df14c409
55555martin <zemanm@gchd.cz>
2025-09-08 10:40:53 +0300

    Úvodní hodina

---
+++ 7M/pratelstvi/pratelstvi/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratelstvi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine()); // Počet lidí
+            List<int>[] kamaradi = new List<int>[n+1];
+            for (int x = 0; x < n+1; x++) // n+1 je protože lidé jsou indexováni od 1 do n, nikoliv od 0 do n-1, jak to funguje v prog. jazycích
+            {
+                kamaradi[x] = new List<int>();
+            }
+            string[] pratelstvi = Console.ReadLine().Split(' ');
+            foreach (string x in pratelstvi)
+            {
+                string[] spoj = x.Split('-');
+                int i1 = Int32.Parse(spoj[0]);
+                int i2 = Int32.Parse(spoj[1]);
+                kamaradi[i1].Add(i2);
+                kamaradi[i2].Add(i1);
+            }
+            string[] startcil = Console.ReadLine().Split(' ');
+            int start = Int32.Parse(startcil[0]);
+            int cil = Int32.Parse(startcil[1]);
+            Dictionary<int,bool> projite = new Dictionary<int, bool>();
+            List<int[]> cesty = new List<int[]>();
+            int pocetcest = 0;
+            projite.Add(start,true);
+            foreach (int x in kamaradi[start])
+            {
+                projite.Add(x, true);
+                int[] cesta = new int[2];
+                cesta[0] = start;
+                cesta[1] = x;
+                cesty.Add(cesta);
+                pocetcest++;
+            }
+            int i = 0;
+            int[] konecnaCesta = null;
+            while (i<pocetcest)
+            {
+                int[] cesta = cesty[i];
+                int delka = cesta.Length;
+                int prvek = cesta.Last();
+                foreach (int x in kamaradi[prvek])
+                {
+                    if (!projite.ContainsKey(x))
+                    {
+                        projite.Add(x, true);
+                        int[] novaCesta = new int[delka+1];
+                        for (int y = 0; y < delka; y++)
+                        {
+                            novaCesta[y] = cesta[y];
+                        }
+                        novaCesta[delka] = x;
+                        cesty.Add(novaCesta);
+                        pocetcest++;
+                        if (x == cil)
+                        {
+                            konecnaCesta = novaCesta;
+                            break;
+                        }
+                    }
+                }
+                if (konecnaCesta != null)
+                    break;
+                i++;
+            }
+            if (konecnaCesta == null)
+                Console.WriteLine("neexistuje");
+            else
+            {
+                foreach (int x in konecnaCesta)
+                {
+                    Console.Write(x.ToString()+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        } // časová složitost O(n^2)
+    }     // paměťová složitost string[] pratelstvi-O(m); List<int>[] kamaradi-O(2m); List<int> projite-O(n); List<int[]> cesty-O(n*n)
+}         //                    celkem -> O(n^2+m)
---
+++ 8M/uvodni_hodina/uvodni_hodina/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodni_hodina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Film film1 = new Film("Ten nejlepší film pod sluncem", "Martin", "Zeman", 2025);
+            Film film2 = new Film("Taky dobrej film", "Pepa", "Omáčka", 2007);
+            Film film3 = new Film("Filmíček", "Jan", "Novák", 2031);
+            List<Film> filmy = new List<Film>() {film1, film2, film3};
+            for (int i = 0; i < filmy.Count; i++)
+            {
+                Film film = filmy[i];
+                for (int j = 0; j < 15; j++)
+                {
+                    Random random = new Random();
+                    double cislo = random.NextDouble() * 5;
+                    film.PridatHodnoceni(cislo);
+                }
+            }
+            Film nejHodnoceni = null;
+            Film nejdNazev = null;
+            foreach (Film film in filmy)
+            {
+                if (nejHodnoceni == null)
+                {
+                    nejHodnoceni = film;
+                    nejdNazev = film;
+                }
+                else if (film.Hodnoceni > nejHodnoceni.Hodnoceni)
+                {
+                    nejHodnoceni = film;
+                }
+                if (film.Nazev.Length > nejdNazev.Nazev.Length)
+                {
+                    nejdNazev = film;
+                }
+                if (film.Hodnoceni < 3)
+                {
+                    Console.WriteLine($"{film.Nazev} je odpad! Má hodnocení jen {film.Hodnoceni}.");
+                }
+            }
+        }
+    }
+
+    public class Film
+    {
+        public string Nazev { get; }
+        public string JmenoRezisera { get; }
+        public string PrijmeniRezisera { get; }
+        public int RokVzniku { get; }
+        public double Hodnoceni { get; private set; }
+        public List<double> SeznamHodnoceni { get; private set; }
+        public Film(string n, string jr, string pr, int rv)
+        {
+            Nazev = n;
+            JmenoRezisera = jr;
+            PrijmeniRezisera = pr;
+            RokVzniku = rv;
+            Hodnoceni = 0;
+            SeznamHodnoceni = new List<double>();
+        }
+        public void PridatHodnoceni(double h)
+        {
+            int pocet = SeznamHodnoceni.Count;
+            SeznamHodnoceni.Add(h);
+            Hodnoceni = (pocet * Hodnoceni + h) / (pocet + 1);
+        }
+        public void VypsatHodnoceni()
+        {
+            string vypis = String.Join("⭐\n", SeznamHodnoceni.ToArray());
+            Console.WriteLine("Seznam hodnocení:\n" + vypis + "⭐");
+        }
+        public override string ToString()
+        {
+            char inicial = JmenoRezisera[0];
+            return $"{Nazev} ({RokVzniku}; {PrijmeniRezisera}, {inicial}): {Hodnoceni} ⭐";
+        }
+    }
+}
