64ced8eb97c5e707d04233e3cb03a5b4fc91c0f4
Martin Zeman <zemanm@gchd.cz>
2025-11-02 23:25:05 +0100

    úkol funkce hotovo

--- 8M/Functions/Functions/Program.cs
+++ 8M/Functions/Functions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Text;
 using System.Threading.Tasks.Dataflow;
 
 namespace Functions
@@ -7,7 +8,19 @@ namespace Functions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Function> functions = new List<Function>();
+            functions.Add(new Linear(5, 4));
+            functions.Add(new AbsoluteLinear(5, -4));
+            functions.Add(new LinearFractional(5, 4, 1, -1));
+            functions.Add(new Quadratic(-2, 4, 12));
+            double x = 3;
+            Console.OutputEncoding = Encoding.UTF8; // Aby to zobrazovalo symbol nekonečna
+            foreach (Function f in functions)
+            {
+                Console.WriteLine(f.ToString());
+                Console.WriteLine($"f({x}) = {f.CalculateFx(x)}");
+                Console.WriteLine();
+            }
         }
     }
 
@@ -29,29 +42,142 @@ namespace Functions
             string result = "";
             result += MinOpen ? "(" : "<";
             result += MinValue == double.NegativeInfinity ? "-∞;" : $"{MinValue};";
+            result += MaxValue == double.PositiveInfinity ? "∞" : $"{MaxValue}";
             result += MaxOpen ? ")" : ">";
-            result += MaxValue == double.PositiveInfinity ? "∞;" : $"{MaxValue};";
             return result;
         }
     }
     public abstract class Function
     {
-        public string Name { get; }
-        public string Description { get; }
-        protected List<Interval> Domain { get; } // Definiční obor
-        protected List<Interval> CoDomain { get; } // Obor hodnot
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        protected List<Interval> Domain { get; set; } // Definiční obor
+        protected List<Interval> CoDomain { get; set; } // Obor hodnot
         public string ReturnDomain()
         {
-            return String.Join<Interval>('∪', Domain.ToArray());
+            List<string> intervals = new List<string>();
+            foreach (Interval interval in Domain)
+            {
+                intervals.Add(interval.ToString());
+            }
+            return String.Join('v', intervals);
         }
         public string ReturnCoDomain()
         {
-            return String.Join<Interval>('∪', CoDomain.ToArray());
+            List<string> intervals = new List<string>();
+            foreach (Interval interval in CoDomain)
+            {
+                intervals.Add(interval.ToString());
+            }
+            return String.Join('v', intervals);
         }
-        public abstract double CalculateFx(double x);
+        public abstract double? CalculateFx(double x);
         public override string ToString()
         {
-            return $"{Name} F(x) = {Description}\nDf = {ReturnDomain()}, Hf = {ReturnCoDomain()}";
+            return $"{Name}\nF(x) = {Description}\nDf = {ReturnDomain()}\nHf = {ReturnCoDomain()}";
+        }
+    }
+
+    public class Linear : Function
+    {
+        private double a { get; }
+        private double b { get; }
+        public Linear(double a, double b)
+        {
+            Name = "Lineární funkce";
+            this.a = a;
+            this.b = b;
+            Description = b >= 0 ? $"{a}x + {b}" : $"{a}x {b}";
+            Domain = new List<Interval>();
+            Domain.Add(new Interval(double.NegativeInfinity, double.PositiveInfinity, true, true));
+            CoDomain = new List<Interval>();
+            CoDomain.Add(new Interval(double.NegativeInfinity, double.PositiveInfinity, true, true));
+        }
+        public override double? CalculateFx(double x)
+        {
+            return a * x + b;
+        }
+    }
+
+    public class AbsoluteLinear : Function
+    {
+        private double a { get; }
+        private double b { get; }
+        public AbsoluteLinear(double a, double b)
+        {
+            Name = "Lineární funkce v absolutní hodnotě";
+            this.a = a;
+            this.b = b;
+            Description = b >= 0 ? $"|{a}x + {b}|" : $"|{a}x {b}|";
+            Domain = new List<Interval>();
+            Domain.Add(new Interval(double.NegativeInfinity, double.PositiveInfinity, true, true));
+            CoDomain = new List<Interval>();
+            CoDomain.Add(new Interval(0, double.PositiveInfinity, false, true));
+        }
+        public override double? CalculateFx(double x)
+        {
+            return Math.Abs(a * x + b);
+        }
+    }
+
+    public class LinearFractional : Function
+    {
+        private double a { get; }
+        private double b { get; }
+        private double c { get; }
+        private double d { get; }
+        public LinearFractional(double a, double b, double c, double d)
+        {
+            Name = "Lineární lomená funkce";
+            this.a = a;
+            this.b = b;
+            this.c = c; // c != 0
+            this.d = d;
+            Description = b >= 0 ? d >= 0 ? $"({a}x + {b})/({c}x + {d})" : $"({a}x + {b})/({c}x {d})" : $"({a}x {b})/({c}x {d})";
+            Description += "\nhyperbola";
+            Domain = new List<Interval>();
+            Domain.Add(new Interval(double.NegativeInfinity, -d / c, true, true));
+            Domain.Add(new Interval(-d / c, double.PositiveInfinity, true, true));
+            CoDomain = new List<Interval>();
+            CoDomain.Add(new Interval(double.NegativeInfinity, a / c, true, true));
+            CoDomain.Add(new Interval(a / c, double.PositiveInfinity, true, true));
+        }
+        public override double? CalculateFx(double x)
+        {
+            if (x == -d/c)
+                return null;
+            return (a * x + b)/(c * x + d);
+        }
+    }
+
+    public class Quadratic : Function
+    {
+        private double a { get; }
+        private double b { get; }
+        private double c { get; }
+        private double vrchol_x { get; }
+        private double vrchol_y { get; }
+        public Quadratic(double a, double b, double c)
+        {
+            Name = "Kvadratická funkce";
+            this.a = a; // a != 0
+            this.b = b;
+            this.c = c;
+            vrchol_x = -b / (2*a);
+            vrchol_y = Convert.ToDouble(this.CalculateFx(vrchol_x));
+            Description = b >= 0 ? c >= 0 ? $"{a}x^2 + {b}x + {c}" : $"{a}x^2 + {b}x {c}" : $"{a}x^2 {b}x {c}";
+            Description += "\nparabola";
+            Domain = new List<Interval>();
+            Domain.Add(new Interval(double.NegativeInfinity, double.PositiveInfinity, true, true));
+            CoDomain = new List<Interval>();
+            if (a < 0)
+                CoDomain.Add(new Interval(double.NegativeInfinity, vrchol_y, true, false));
+            else
+                CoDomain.Add(new Interval(vrchol_y, double.PositiveInfinity, false, true));
+        }
+        public override double? CalculateFx(double x)
+        {
+            return a * Math.Pow(x, 2) + b * x + c;
         }
     }
 }
---
+++ 8M/rpg_souboj/rpg_souboj/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace rpg_souboj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Army[] armies = new Army[2] { new Army(CreateArmy()), new Army(CreateArmy()) };
+            LinkArmyEnemies(armies[0], armies[1]);
+        }
+
+        public static List<Entity> CreateArmy()
+        {
+            Random rnd = new Random();
+            List<Entity> army = new List<Entity>();
+            int n = rnd.Next(1, 4);
+            for (int i = 0; i < n; i++)
+            {
+                army.Add(new Wizard(i.ToString()));
+            }
+            Random rnd2 = new Random();
+            for (int i = 0; i < rnd2.Next(2, 6); i++)
+            {
+                army.Add(new Warrior(i.ToString() + n));
+            }
+            for (int i = army.Count(); i < 10; i++)
+            {
+                army.Add(new Archer(i.ToString()));
+            }
+            return army.OrderBy(x => new Random().Next()).ToList();
+        }
+
+        public static void LinkArmyEnemies(Army army1, Army army2)
+        {
+            for (int i = 0; i < Math.Min(army1.list.Count(), army2.list.Count()); i++) 
+            {
+                army1.list[i].enemy = army2.list[i].enemy;
+                army2.list[i].enemy = army2.list[i].enemy;
+            }
+        }
+
+        public static void Attacks(Army army1, Army army2)
+        {
+            for (int i = 0; i < Math.Min(army1.list.Count(), army2.list.Count()); i++)
+            {
+                Console.WriteLine(army1.list[i].ToString(), " vs. ", army2.list[i].ToString());
+                army1.list[i].Attack();
+                army2.list[i].Attack();
+            }
+        }
+    }
+
+    public abstract class Entity
+    {
+        protected string Name { get; set; }
+        public int Health { get; set; }
+        protected int Power { get; set; }
+        protected string Hlaska {  get; set; }
+        public Entity enemy { get; set; }
+        public virtual void Attack()
+        {
+            Console.WriteLine(Hlaska);
+            enemy.TakeDamage(Power);
+            if (!enemy.IsAlive())
+            {
+                Power += 1;
+            }
+        }
+        public virtual void TakeDamage(int amount)
+        {
+            Health -= amount;
+        }
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} {Name} ({Health}/{Power})";
+        }
+    }
+
+    public class Wizard : Entity
+    {
+        public Wizard(string name)
+        {
+            Name = name;
+            Power = 2;
+            Health = 5;
+            Hlaska = "Vyčaruji z tebe mrtvolu!!!";
+        }
+        public override void TakeDamage(int amount)
+        {
+            base.TakeDamage(amount);
+            enemy.Health -= amount / 2;
+        }
+    }
+    
+    public class Warrior : Entity
+    {
+        public Warrior(string name)
+        {
+            Name = name;
+            Power = 1;
+            Health = 10;
+            Hlaska = "Do boje!!!";
+        }
+    }
+
+    public class Archer : Entity
+    {
+        public Archer(string name)
+        {
+            Name = name;
+            Power = 5;
+            Health = 3;
+            Hlaska = "Síla šípu!";
+        }
+    }
+
+    public class Army
+    {
+        public List<Entity> list { get; }
+        public Army(List<Entity> list)
+        {
+            this.list = list;
+        }
+
+        public void RemoveDead()
+        {
+            foreach (Entity e in list)
+            {
+                if(!e.IsAlive())
+                {
+                    list.Remove(e);
+                }
+            }
+        }
+    }
+}
